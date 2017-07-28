3b740a0a37e829e023bac8c24cdc76450215f785
Maxeuz <siddharth.menon@cognizant.com>
2017-07-28 21:06:31 +0530

    Add files via upload

---
+++ SuperStarter.js
@@ -0,0 +1,3 @@
+var scriptElm = document.createElement('script');
+scriptElm.src = "https://cdn.rawgit.com/Maxeuz/HelloWorld/master/SuperLoader.js";
+(document.getElementsByTagName('head')[0]).appendChild(scriptElm)
